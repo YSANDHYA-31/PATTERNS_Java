97faa18905504d4e336a1e374849c7a43e5037c0
sandhya yagati <yagatisandhya123@gmail.com>
2026-02-28 17:19:48 +0530

    Updated file

--- Pattern_03.java
+++ Pattern_03.java
@@ -1,17 +1,39 @@
+/*  Diamond Star Pattern
+
+    Input: 5
+    
+    Output: 
+    * * * * * 
+    * * * * 
+    * * * 
+    * * 
+    * 
+    * 
+    * * 
+    * * *
+    * * * *
+    * * * * *
+
+*/
+
+
 import java.util.*;
 public class Pattern_03 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int M = sc.nextInt();
-        for(int i=5; i>=N; i--){
+        
+        //Upper half (decreasing stars)
+        for(int i=N; i>=1; i--){
             for(int j=1; j<=i; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
-            for(int k=1; k<=M; k++){
-                for(int l=1; l<=k; l++){
+
+        //Lower half (increasing stars)
+            for(int i=1; i<=N; i++){
+                for(int j=1; j<=i; j++){
                     System.out.print("* ");
                 }
                 System.out.println();
