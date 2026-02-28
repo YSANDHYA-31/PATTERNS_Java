a60a779d50d95e28671af34f6133a18e7b984df0
sandhya yagati <yagatisandhya123@gmail.com>
2026-02-28 13:23:50 +0530

    Patterns

---
+++ Pattern_04.java
@@ -0,0 +1,26 @@
+
+/* 
+    Input: 5
+     
+    Output:
+     
+    * * * * * 
+    * * * * *
+    * * * * *
+    * * * * *
+    * * * * *
+*/
+
+import java.util.*;
+public class Pattern_04 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
